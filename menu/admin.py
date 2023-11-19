0bb1e17662a8d16ef18684c50a05d1715cc4a8cc
Ekaterina Fedoseeva <kaitherinayrd@gmail.com>
2023-11-20 00:24:44 +0100

    feat: slug added to models, migrations reconfigured

--- menu/admin.py
+++ menu/admin.py
@@ -4,4 +4,6 @@ from .models import MenuItem
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url', 'parent')
+    list_display = ('id', 'name', 'category', 'slug', 'parent', 'url')
+    list_filter = ('category', 'parent')
+    prepopulated_fields = {'slug': ('name', )}
--- menu/models.py
+++ menu/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, null=True)
+    slug = models.SlugField(unique=True, null=True)
     url = models.CharField(max_length=255)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
